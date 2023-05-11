b5a31838aa3409c59ecbc6d195e493229bf66ed2
FLX777 <subtirica.florin29@gmail.com>
2023-05-11 09:41:50 +0300

    Register and Login implemented

--- client.c
+++ client.c
@@ -6,82 +6,212 @@
 #include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
 #include <netdb.h>      /* struct hostent, gethostbyname */
 #include <arpa/inet.h>
+#include <stdbool.h>
 #include "helpers.h"
 #include "requests.h"
+#include "parson.h"
+
+#define HOST "34.254.242.81"
+#define PORT 8080
+
+#define POST_REGISTER   "/api/v1/tema/auth/register"
+#define POST_LOGIN      "/api/v1/tema/auth/login"
+#define GET_ACCESS      "/api/v1/tema/library/access"
+#define GET_BOOKS       "/api/v1/tema/library/books"
+// #define GET_BOOKID      "/api/v1/tema/library/books/:bookId."
+#define POST_BOOK       "/api/v1/tema/library/books"
+// #define DELETE_BOOKID   "/api/v1/tema/library/books/:bookId."
+#define GET_LOGOUT      "/api/v1/tema/auth/logout"
+
+#define BUFFER_SIZE     100
+
+char *message;
+char *response;
+int sockfd;
+
+char *buffer;
+
+// Exit condition
+bool leave = false;
+bool login = false;
+
+char **cookie;
+char *token;
+
+char *json_input_parsing(char *serialized_string) {
+    char *username = calloc(BUFFER_SIZE, sizeof(char));
+    char *password = calloc(BUFFER_SIZE, sizeof(char));
+
+    user:
+    printf("username=");
+    fgets(username, BUFFER_SIZE, stdin);
+
+    if (strncmp(username, "\n", 1) == 0) {
+        printf("User does not exist\n");
+        goto user;
+    }
+
+    printf("password=");
+    fgets(password, BUFFER_SIZE, stdin);
+
+    if (strncmp(password, "\n", 1) == 0) {
+        printf("Password does not exist\n");
+        goto user;
+    }
+
+    strtok(username, "\n");
+    strtok(password, "\n");
+
+    JSON_Value *root_value = json_value_init_object();
+    JSON_Object *root_object = json_value_get_object(root_value);
+
+    json_object_set_string(root_object, "username", username);
+    json_object_set_string(root_object, "password", password);
+
+    serialized_string = json_serialize_to_string_pretty(root_value);
+}
+
+void post_register() {
+    char *serialized_string = NULL;
+    json_input_parsing(serialized_string);
+
+    message = compute_post_request(HOST, POST_REGISTER, "application/json", serialized_string, 0, NULL, 0);
+
+    send_to_server(sockfd, message);
+    response = receive_from_server(sockfd);
+
+    printf("%s\n", response);
+}
+
+void post_login() {
+    if (login) {
+        printf("You are already logged in\n");
+
+        return;
+    }
+
+    char *serialized_string = NULL;
+    json_input_parsing(serialized_string);
+
+    message = compute_post_request(HOST, POST_LOGIN, "application/json", &serialized_string, NULL, NULL, 0);
+
+    send_to_server(sockfd, message);
+    response = receive_from_server(sockfd);
+
+    printf("%s\n", response);
+
+    char **cookies;
+    char *rest = response;
+
+    while ((cookies = strtok_r(rest, "\n", &rest))) {
+        if (strstr(cookies, "Set-Cookie:")) {
+            cookie = strtok(cookies, ";");
+            cookie = strtok(cookie, ":");
+            cookie = strtok(0, " ");
+
+            break;
+        }
+    }
+
+    login = true;
+}
+
+void get_access() {
+
+}
+
+void get_books() {
+
+}
+
+void get_bookid() {
+
+}
+
+void post_book() {
+
+}
+
+void delete_bookid(int id) {
+
+}
+
+void get_logout() {
+    message = compute_get_request(HOST, GET_LOGOUT, NULL, cookie, 0);
+
+    send_to_server(sockfd, message);
+    response = receive_from_server(sockfd);
+
+    printf("%s\n", response);
+
+    cookie = NULL;
+    token = NULL;
+
+    login = false;
+}
+
+void default_case() {
+    printf("Bad command \"%s\"\n\n", buffer);
+    printf("Available commands:\n");
+    printf("register\nlogin\nenter_library\nget_books\nget_book\nadd_book\ndelete_book\nlogout\nexit\n");
+    printf("\nPlease enter one of these\n");
+}
 
 int main(int argc, char *argv[])
 {
-    char *message;
-    char *response;
-    int sockfd;
+    sockfd = open_connection(HOST, PORT, AF_INET, SOCK_STREAM, 0);
 
-    // Ex 1.1: GET dummy from main server
+    while(!leave) {
+        buffer = calloc(BUFFER_SIZE, sizeof(char));
+        fgets(buffer, BUFFER_SIZE, stdin);
+        strtok(buffer, "\n");
 
-    char *host = "34.254.242.81";
-    int port = 8080;
-    char *host_port = "34.254.242.81:8080";
-    char *url = "/api/v1/dummy";
-    char *query_params = NULL;
-    char **cookies = NULL;
-    int cookies_count = 0;
+        switch (buffer[0]) {
+        case 'r':
+            if (strncmp(buffer, "register", 8) == 0) post_register();
+            else default_case;
+            
+            break;
+        case 'l':
+            if (strncmp(buffer, "login", 5) == 0) post_login();
+            else if (strncmp(buffer, "logout", 6) == 0) get_logout();
+            else default_case;
 
-    sockfd = open_connection(host, port, AF_INET, SOCK_STREAM, 0);
+            break;
+        case 'e':
+            if (strncmp(buffer, "enter_library", 13) == 0) get_access();
+            else if (strncmp(buffer, "exit", 4) == 0) leave = true;
+            else default_case;
+            
+            break;
+        case 'g':
+            if (strncmp(buffer, "get_books", 9) == 0) get_books();
+            else if (strncmp(buffer, "get_book", 8) == 0) get_bookid();
+            else default_case;
+            
+            break;
+        case 'a':
+            if (strncmp(buffer, "add_book", 8) == 0) post_book();
+            else default_case;
+            
+            break;
+        case 'd':
+            if (strncmp(buffer, "delete_book", 11) == 0) {
+                char *id = strtok(buffer, " ");
+                delete_bookid(id);
+            } else default_case;
 
-    message = compute_get_request(host_port, url, query_params, cookies, cookies_count);
-    send_to_server(sockfd, message);
-    response = receive_from_server(sockfd);
-    printf("%s\n", response);
+            break;
+        default:
+            default_case;
 
-    // Ex 1.2: POST dummy and print response from main server
+            break;
+        }
 
-    char *content_type = "application/x-www-form-urlencoded";
+        close_connection(sockfd);
+    }
 
-    char *body_data[2];
-    body_data[0] = calloc(strlen("a=1&b=2") + 1, sizeof(char));
-    body_data[1] = calloc(strlen("a=1&b=2") + 1, sizeof(char));
-    strcpy(body_data[0], "a=1&b=2");
-    strcpy(body_data[1], "a=1&b=2");
-
-    message = compute_post_request(host_port, url, content_type, body_data, 2, NULL, 0);
-    send_to_server(sockfd, message);
-    response = receive_from_server(sockfd);
-    printf("%s\n", response);
-
-    // Ex 2: Login into main server
-
-    char *url_login = "/api/v1/auth/login";
-
-    char *body_data_login[2];
-    body_data_login[0] = calloc(strlen("username=student&password=student") + 1, sizeof(char));
-    body_data_login[1] = calloc(strlen("username=student&password=student") + 1, sizeof(char));
-    strcpy(body_data_login[0], "username=student&password=student");
-    strcpy(body_data_login[1], "username=student&password=student");
-
-    message = compute_post_request(host_port, url_login, content_type, body_data_login, 2, NULL, 0);
-    send_to_server(sockfd, message);
-    response = receive_from_server(sockfd);
-    printf("%s\n", response);
-
-    // Ex 3: GET weather key from main server
-
-
-
-    // Ex 4: GET weather data from OpenWeather API
-
-    
-
-    // Ex 5: POST weather data for verification to main server
-
-    
-
-    // Ex 6: Logout from main server
-
-   
-
-    // BONUS: make the main server return "Already logged in!"
-
-    // free the allocated data at the end!
-
+    free(buffer);
     free(message);
     free(response);
     close_connection(sockfd);
--- requests.c
+++ requests.c
@@ -13,9 +13,10 @@ char *compute_get_request(char *host, char *url, char *query_params,
                             char **cookies, int cookies_count)
 {
     char *message = calloc(BUFLEN, sizeof(char));
-    char *line = calloc(LINELEN, sizeof(char));
+    char *line    = calloc(LINELEN, sizeof(char));
 
     // Step 1: write the method name, URL, request params (if any) and protocol type
+
     if (query_params != NULL) {
         sprintf(line, "GET %s?%s HTTP/1.1", url, query_params);
     } else {
@@ -25,11 +26,19 @@ char *compute_get_request(char *host, char *url, char *query_params,
     compute_message(message, line);
 
     // Step 2: add the host
+
+    sprintf(line, "Host: %s", host);
+    compute_message(message, line);
+
     // Step 3 (optional): add headers and/or cookies, according to the protocol format
+
     if (cookies != NULL) {
-       
+        sprintf(line, "Cookie: %s", cookies);
+        compute_message(message, line);
     }
     // Step 4: add final new line
+
+    sprintf(line, "");
     compute_message(message, "");
     return message;
 }
@@ -42,20 +51,39 @@ char *compute_post_request(char *host, char *url, char* content_type, char **bod
     char *body_data_buffer = calloc(LINELEN, sizeof(char));
 
     // Step 1: write the method name, URL and protocol type
+
     sprintf(line, "POST %s HTTP/1.1", url);
     compute_message(message, line);
     
     // Step 2: add the host
+
+    sprintf(line, "Host: %s", host);
+    compute_message(message, line);
+
     /* Step 3: add necessary headers (Content-Type and Content-Length are mandatory)
             in order to write Content-Length you must first compute the message size
     */
+
+    sprintf(line, "Content-Type: %s", content_type);
+    compute_message(message, line);
+
+    sprintf(line, "Content-Length: %ld", strlen(body_data));
+    compute_message(message, line);
+
     // Step 4 (optional): add cookies
+
     if (cookies != NULL) {
-       
+        sprintf(line, "Cookies: %s", cookies);
+        compute_message(message, line); 
     }
+
     // Step 5: add new line at end of header
 
+    sprintf(line, "");
+    compute_message(message, line);
+
     // Step 6: add the actual payload data
+    
     memset(line, 0, LINELEN);
     strcat(message, body_data_buffer);
 
